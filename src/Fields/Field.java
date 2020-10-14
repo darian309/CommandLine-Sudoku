2d20e4501a3672eb5a2a4299504b8276e56f63f5
Diego-VP20 <69905156+Diego-VP20@users.noreply.github.com>
2020-10-14 15:09:59 +0200

    Update

--- src/Fields/Field.java
+++ src/Fields/Field.java
@@ -56,16 +56,54 @@ public class Field {
         char X = Location.charAt(1);
         int x = Character.getNumericValue(X);
         int y = Character.getNumericValue(Y);
+        int quadrant = 0;
         int[] result = new int[2];
 
+        if(x < 3 && y < 3){
 
-        //Return Quadrant
-        result[0] = x/3;
-        if(y>3||y<7){result[0] = result[0]*2;};
-        if(y>6||y<9){result[0] = result[0]*3;};
+            quadrant = 0;
 
-        //return field
-        result[1] = (x/3)+((y/3));
+        }else if(x > 2  && x < 6 && y < 3){
+
+            quadrant = 1;
+
+        }else if(x > 5 && x <= 8 && y < 3){
+
+            quadrant = 2;
+
+        }else if (x < 3  && y < 6){
+
+            quadrant = 3;
+
+        }else if(x < 6  && x > 2 && y > 2 && y < 6){
+
+            quadrant = 4;
+
+        }else if(x > 5 && x < 9 && y > 2 && y < 6){
+
+            quadrant = 5;
+
+        }else if(x < 3 && y > 6 && y < 9){
+
+            quadrant = 6;
+
+        }else if(x > 2 && x < 6 && y > 5 && y < 9){
+
+            quadrant = 7;
+
+        }else if(x > 5 && x < 9 && y > 6 && y < 9){
+
+            quadrant = 8;
+
+        }else{
+
+            System.out.println("ERROR 100 FIELD");
+
+        }
+
+
+        result[0] = quadrant;
+        result[1] = ((x/3)*(y/3)-1);
         return result;
     }
 
