99acffb428fc2293222822e34ab926d8b8dea43c
Arkki7002 <Darzibas@gmail.com>
2020-10-14 13:33:22 +0200

    Update Locate Field Function

--- src/Fields/Field.java
+++ src/Fields/Field.java
@@ -65,7 +65,7 @@ public class Field {
         if(y>6||y<9){result[0] = result[0]*3;};
 
         //return field
-        result[1] = (x/3)+((y/3)-1);
+        result[1] = (x/3)+((y/3));
         return result;
     }
 
