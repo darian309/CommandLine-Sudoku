9b307b4a5e11211e7e2c7fdd23bbca70a86c7a22
Diego-VP20 <diego.varela@stud.edubs.ch>
2020-10-14 17:18:43 +0200

    Fix syntax

--- src/Fields/Field.java
+++ src/Fields/Field.java
@@ -59,52 +59,52 @@ public class Field {
         int quadrant = 0;
         int[] result = new int[2];
 
-        if(x < 3 && y < 3){
-
-            quadrant = 0;
-
-        }else if(x > 2  && x < 6 && y < 3){
+        if (x > 2 && x < 6 && y < 3) {
 
             quadrant = 1;
 
-        }else if(x > 5 && x <= 8 && y < 3){
+        } else if (x > 5 && x <= 8 && y < 3) {
 
             quadrant = 2;
 
-        }else if (x < 3  && y < 6){
+        } else if (x < 3 && y < 6) {
 
             quadrant = 3;
 
-        }else if(x < 6  && x > 2 && y > 2 && y < 6){
+        } else if (x < 6 && x > 2 && y > 2 && y < 6) {
 
             quadrant = 4;
 
-        }else if(x > 5 && x < 9 && y > 2 && y < 6){
+        } else if (x > 5 && x < 9 && y > 2 && y < 6) {
 
             quadrant = 5;
 
-        }else if(x < 3 && y > 6 && y < 9){
+        } else if (x < 3 && y > 6 && y < 9) {
 
             quadrant = 6;
 
-        }else if(x > 2 && x < 6 && y > 5 && y < 9){
+        } else if (x > 2 && x < 6 && y > 5 && y < 9) {
 
             quadrant = 7;
 
-        }else if(x > 5 && x < 9 && y > 6 && y < 9){
+        } else if (x > 5 && x < 9 && y > 6 && y < 9) {
 
             quadrant = 8;
 
+
+        }
+
         //return field
-        result[1] = (x+((y-1)*3))-1;
+        result[1] = (x + ((y - 1) * 3)) - 1;
         return result;
+
     }
 
     public static String ArrayToUserLocation(int x){
 
         String xyCoords;
 
-        switch (x){
+        switch (x) {
 
             case 0:
                 xyCoords = "00";
@@ -409,33 +409,87 @@ public class Field {
         int[] values = new int[81];
         int i;
         // put values of Fields in a Ro for Row order
-        for(i= 0;i<= 2;i++){values[i] = Field[0].getValue(/*      */i);}//
-        for(i= 3;i<= 5;i++){values[i] = Field[1].getValue(i- 3);}
-        for(i= 6;i<= 8;i++){values[i] = Field[2].getValue(i- 6);}
-        for(i= 9;i<=11;i++){values[i] = Field[0].getValue(i- 6);}
-        for(i=12;i<=14;i++){values[i] = Field[1].getValue(i- 9);}
-        for(i=15;i<=17;i++){values[i] = Field[2].getValue(i-12);}
-        for(i=18;i<=20;i++){values[i] = Field[0].getValue(i-12);}
-        for(i=21;i<=23;i++){values[i] = Field[1].getValue(i-15);}
-        for(i=24;i<=26;i++){values[i] = Field[2].getValue(i-18);}
-        for(i=27;i<=29;i++){values[i] = Field[3].getValue(i-27);}//
-        for(i=30;i<=32;i++){values[i] = Field[4].getValue(i-30);}
-        for(i=33;i<=35;i++){values[i] = Field[5].getValue(i-33);}
-        for(i=36;i<=38;i++){values[i] = Field[3].getValue(i-33);}
-        for(i=39;i<=41;i++){values[i] = Field[4].getValue(i-36);}
-        for(i=42;i<=44;i++){values[i] = Field[5].getValue(i-39);}
-        for(i=45;i<=47;i++){values[i] = Field[3].getValue(i-39);}
-        for(i=48;i<=50;i++){values[i] = Field[4].getValue(i-42);}
-        for(i=51;i<=53;i++){values[i] = Field[5].getValue(i-45);}
-        for(i=54;i<=56;i++){values[i] = Field[6].getValue(i-51);}//
-        for(i=57;i<=59;i++){values[i] = Field[7].getValue(i-57);}
-        for(i=60;i<=62;i++){values[i] = Field[8].getValue(i-60);}
-        for(i=63;i<=65;i++){values[i] = Field[6].getValue(i-60);}
-        for(i=66;i<=68;i++){values[i] = Field[7].getValue(i-63);}
-        for(i=69;i<=71;i++){values[i] = Field[8].getValue(i-66);}
-        for(i=72;i<=74;i++){values[i] = Field[6].getValue(i-66);}
-        for(i=75;i<=77;i++){values[i] = Field[7].getValue(i-69);}
-        for(i=78;i<=80;i++){values[i] = Field[8].getValue(i-72);} //
+        for (i = 0; i <= 2; i++) {
+            values[i] = Field[0].getValue(/*      */i);
+        }//
+        for (i = 3; i <= 5; i++) {
+            values[i] = Field[1].getValue(i - 3);
+        }
+        for (i = 6; i <= 8; i++) {
+            values[i] = Field[2].getValue(i - 6);
+        }
+        for (i = 9; i <= 11; i++) {
+            values[i] = Field[0].getValue(i - 6);
+        }
+        for (i = 12; i <= 14; i++) {
+            values[i] = Field[1].getValue(i - 9);
+        }
+        for (i = 15; i <= 17; i++) {
+            values[i] = Field[2].getValue(i - 12);
+        }
+        for (i = 18; i <= 20; i++) {
+            values[i] = Field[0].getValue(i - 12);
+        }
+        for (i = 21; i <= 23; i++) {
+            values[i] = Field[1].getValue(i - 15);
+        }
+        for (i = 24; i <= 26; i++) {
+            values[i] = Field[2].getValue(i - 18);
+        }
+        for (i = 27; i <= 29; i++) {
+            values[i] = Field[3].getValue(i - 27);
+        }//
+        for (i = 30; i <= 32; i++) {
+            values[i] = Field[4].getValue(i - 30);
+        }
+        for (i = 33; i <= 35; i++) {
+            values[i] = Field[5].getValue(i - 33);
+        }
+        for (i = 36; i <= 38; i++) {
+            values[i] = Field[3].getValue(i - 33);
+        }
+        for (i = 39; i <= 41; i++) {
+            values[i] = Field[4].getValue(i - 36);
+        }
+        for (i = 42; i <= 44; i++) {
+            values[i] = Field[5].getValue(i - 39);
+        }
+        for (i = 45; i <= 47; i++) {
+            values[i] = Field[3].getValue(i - 39);
+        }
+        for (i = 48; i <= 50; i++) {
+            values[i] = Field[4].getValue(i - 42);
+        }
+        for (i = 51; i <= 53; i++) {
+            values[i] = Field[5].getValue(i - 45);
+        }
+        for (i = 54; i <= 56; i++) {
+            values[i] = Field[6].getValue(i - 51);
+        }//
+        for (i = 57; i <= 59; i++) {
+            values[i] = Field[7].getValue(i - 57);
+        }
+        for (i = 60; i <= 62; i++) {
+            values[i] = Field[8].getValue(i - 60);
+        }
+        for (i = 63; i <= 65; i++) {
+            values[i] = Field[6].getValue(i - 60);
+        }
+        for (i = 66; i <= 68; i++) {
+            values[i] = Field[7].getValue(i - 63);
+        }
+        for (i = 69; i <= 71; i++) {
+            values[i] = Field[8].getValue(i - 66);
+        }
+        for (i = 72; i <= 74; i++) {
+            values[i] = Field[6].getValue(i - 66);
+        }
+        for (i = 75; i <= 77; i++) {
+            values[i] = Field[7].getValue(i - 69);
+        }
+        for (i = 78; i <= 80; i++) {
+            values[i] = Field[8].getValue(i - 72);
+        } //
         return values;
     }
 
